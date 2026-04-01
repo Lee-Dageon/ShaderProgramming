4cbe26ea5f0fea10668302d0ff8a2884d1a4988a
da-geonLee <leedageon764@gmail.com>
2026-04-01 16:22:34 +0900

    feat: 격자가 꽉차게

--- SimpleGame/Shaders/Fs.fs
+++ SimpleGame/Shaders/Fs.fs
@@ -21,11 +21,11 @@ void Simple()
 void Line()
 {
 	//FragColor = vec4(v_Tex.xy, 0, 1);
-	float periodX = v_Tex.x * 2 * PI * 5;
-	float periodY = v_Tex.y * 2 * PI * 5;
+	float periodX = v_Tex.x * 2 * PI * 5 + 0.5 *PI;
+	float periodY = v_Tex.y * 2 * PI * 5+ 0.5 * PI;
 	float valueX = pow(abs(sin(periodX)), 16);
 	float valueY = pow(abs(sin(periodY)), 16);
-	FragColor = vec4(max(valueX, valueY));
+	FragColor = FragColor = vec4(max(valueX, valueY));
 }
 
 void main()
