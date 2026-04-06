7b2e923c8cb013d9d667cb294ce46ae6696f6054
da-geonLee <leedageon764@gmail.com>
2026-04-06 17:57:41 +0900

    feat: range가 점점 증가하게 해보자
    1.

--- SimpleGame/Shaders/Fs.fs
+++ SimpleGame/Shaders/Fs.fs
@@ -50,13 +50,15 @@ void Circle()
 
 void RainDrop()
 {
+	float newTime = fract(u_Time);	// 0~1
+
 	vec2 center = vec2(0.5, 0.5);
 	vec2 currPos = v_Tex;
 	float dist = distance(center, currPos);
 	float width = 0.01;
 	float radius = 0.5;
 	float count = 15;
-	float range = 0.2;
+	float range = newTime;
 
 	float fade = (1/range) * clamp(range - dist, 0, 1);
 	float grey = fade * pow(abs(sin(dist * count * PI - u_Time*5)), 32);
